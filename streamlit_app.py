144aa661ac4f71e90234271c499d8478b9de22bf
Augusto Makin <augustomakin@hotmail.com.ar>
2023-06-23 16:29:03 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+streamlit.header('Breakfast Menu')
+streamli.text('Omega 3 and Blueberry Oatmeal')
+streamli.text('Kale, Spinach and Rocket Smoothie')
+streamli.text('Hard-Boiled Free-Range Egg')
